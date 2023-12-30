6c73fb9de92bc50ee2bbf4ceea3a1bc8379af22c
Tomas <tomas.ew88@gmail.com>
2023-12-30 20:37:50 +0200

    .gitignore fixed

--- Engine/src/Components/Mesh.cpp
+++ Engine/src/Components/Mesh.cpp
@@ -8,12 +8,7 @@ namespace Engine3D
 	void Mesh::Start()
 	{
 		// Default triangle
-		vertices =
-		{
-			{ -1.f, -1.f, .0f},
-			{.0f, 1.f, .0f },
-			{1.f, -1.f, .0f}
-		};
+
 		std::vector<unsigned int> indices = { 0, 1, 2 };
 		// Create a vbo and ebo
 		vbo = std::make_unique<VBO>(vertices);
